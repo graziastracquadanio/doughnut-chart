758f3bd175edee0869f729fbe6e219178ae54ed9
Grazia Stracquadanio <graziastracquadanio@gmail.com>
2015-02-28 19:28:05 +0100

    improved custom doughnut chart: restored label drawing, bugfixed draw chart function onload

--- src/Chart.Doughnut.js
+++ src/Chart.Doughnut.js
@@ -80,6 +80,8 @@
 				this.addData(datapoint, index, true);
 			},this);
 
+			this.loaded = false;
+
 			this.render();
 		},
 		getSegmentsAtEvent : function(e){
@@ -134,14 +136,35 @@
 			});
 			this.render();
 		},
-
 		removeData: function(atIndex){
 			var indexToDelete = (helpers.isNumber(atIndex)) ? atIndex : this.segments.length-1;
 			this.segments.splice(indexToDelete, 1);
 			this.reflow();
 			this.update();
 		},
-
+		render : function(reflow){
+			if (reflow){
+				this.reflow();
+			}
+			if (this.options.animation && !reflow){
+				helpers.animationLoop(
+					this.draw,
+					this.options.animationSteps,
+					this.options.animationEasing,
+					this.options.onAnimationProgress,
+					function() {
+						this.loaded = true;
+						this.options.onAnimationComplete;
+					},
+					this
+				);
+			}
+			else{
+				this.draw();
+				this.options.onAnimationComplete.call(this);
+			}
+			return this;
+		},
 		reflow : function(){
 			helpers.extend(this.SegmentArc.prototype,{
 				x : this.chart.width/2,
@@ -156,6 +179,8 @@
 			}, this);
 		},
 		showTooltip : function(ChartElements, forceRedraw){
+			if (this.loaded == false) return;
+
 			// Only redraw the chart if we've actually changed what we're hovering on.
 			if (typeof this.activeElements === 'undefined') this.activeElements = [];
 
@@ -187,119 +212,120 @@
 				10,
 				"linear",
 				this.options.onAnimationProgress,
-				this.options.onAnimationComplete,
+				function() {
+					if(this.options.customTooltips){
+						this.options.customTooltips(false);
+					}
+					if (ChartElements.length > 0){
+						// If we have multiple datasets, show a MultiTooltip for all of the data points at that index
+						if (this.datasets && this.datasets.length > 1) {
+							var dataArray,
+								dataIndex;
+
+							for (var i = this.datasets.length - 1; i >= 0; i--) {
+								dataArray = this.datasets[i].points || this.datasets[i].bars || this.datasets[i].segments;
+								dataIndex = indexOf(dataArray, ChartElements[0]);
+								if (dataIndex !== -1){
+									break;
+								}
+							}
+							var tooltipLabels = [],
+								tooltipColors = [],
+								medianPosition = (function(index) {
+
+									// Get all the points at that particular index
+									var Elements = [],
+										dataCollection,
+										xPositions = [],
+										yPositions = [],
+										xMax,
+										yMax,
+										xMin,
+										yMin;
+									helpers.each(this.datasets, function(dataset){
+										dataCollection = dataset.points || dataset.bars || dataset.segments;
+										if (dataCollection[dataIndex] && dataCollection[dataIndex].hasValue()){
+											Elements.push(dataCollection[dataIndex]);
+										}
+									});
+
+									helpers.each(Elements, function(element) {
+										xPositions.push(element.x);
+										yPositions.push(element.y);
+
+
+										//Include any colour information about the element
+										tooltipLabels.push(helpers.template(this.options.multiTooltipTemplate, element));
+										tooltipColors.push({
+											fill: element._saved.fillColor || element.fillColor,
+											stroke: element._saved.strokeColor || element.strokeColor
+										});
+
+									}, this);
+
+									yMin = min(yPositions);
+									yMax = max(yPositions);
+
+									xMin = min(xPositions);
+									xMax = max(xPositions);
+
+									return {
+										x: (xMin > this.chart.width/2) ? xMin : xMax,
+										y: (yMin + yMax)/2
+									};
+								}).call(this, dataIndex);
+
+							new Chart.MultiTooltip({
+								x: medianPosition.x,
+								y: medianPosition.y,
+								xPadding: this.options.tooltipXPadding,
+								yPadding: this.options.tooltipYPadding,
+								xOffset: this.options.tooltipXOffset,
+								fillColor: this.options.tooltipFillColor,
+								textColor: this.options.tooltipFontColor,
+								fontFamily: this.options.tooltipFontFamily,
+								fontStyle: this.options.tooltipFontStyle,
+								fontSize: this.options.tooltipFontSize,
+								titleTextColor: this.options.tooltipTitleFontColor,
+								titleFontFamily: this.options.tooltipTitleFontFamily,
+								titleFontStyle: this.options.tooltipTitleFontStyle,
+								titleFontSize: this.options.tooltipTitleFontSize,
+								cornerRadius: this.options.tooltipCornerRadius,
+								labels: tooltipLabels,
+								legendColors: tooltipColors,
+								legendColorBackground : this.options.multiTooltipKeyBackground,
+								title: ChartElements[0].label,
+								chart: this.chart,
+								ctx: this.chart.ctx,
+								custom: this.options.customTooltips
+							}).draw();
+
+						} else {
+							helpers.each(ChartElements, function(Element) {
+								var tooltipPosition = Element.tooltipPosition();
+								new Chart.Tooltip({
+									x: Math.round(tooltipPosition.x),
+									y: Math.round(tooltipPosition.y),
+									xPadding: this.options.tooltipXPadding,
+									yPadding: this.options.tooltipYPadding,
+									fillColor: this.options.tooltipFillColor,
+									textColor: this.options.tooltipFontColor,
+									fontFamily: this.options.tooltipFontFamily,
+									fontStyle: this.options.tooltipFontStyle,
+									fontSize: this.options.tooltipFontSize,
+									caretHeight: this.options.tooltipCaretSize,
+									cornerRadius: this.options.tooltipCornerRadius,
+									text: helpers.template(this.options.tooltipTemplate, Element),
+									chart: this.chart,
+									custom: this.options.customTooltips
+								}).draw();
+							}, this);
+						}
+					}
+				},
 				this
 			);
 
-			if(this.options.customTooltips){
-				this.options.customTooltips(false);
-			}
-			if (ChartElements.length > 0){
-				// If we have multiple datasets, show a MultiTooltip for all of the data points at that index
-				if (this.datasets && this.datasets.length > 1) {
-					var dataArray,
-						dataIndex;
-
-					for (var i = this.datasets.length - 1; i >= 0; i--) {
-						dataArray = this.datasets[i].points || this.datasets[i].bars || this.datasets[i].segments;
-						dataIndex = indexOf(dataArray, ChartElements[0]);
-						if (dataIndex !== -1){
-							break;
-						}
-					}
-					var tooltipLabels = [],
-						tooltipColors = [],
-						medianPosition = (function(index) {
-
-							// Get all the points at that particular index
-							var Elements = [],
-								dataCollection,
-								xPositions = [],
-								yPositions = [],
-								xMax,
-								yMax,
-								xMin,
-								yMin;
-							helpers.each(this.datasets, function(dataset){
-								dataCollection = dataset.points || dataset.bars || dataset.segments;
-								if (dataCollection[dataIndex] && dataCollection[dataIndex].hasValue()){
-									Elements.push(dataCollection[dataIndex]);
-								}
-							});
-
-							helpers.each(Elements, function(element) {
-								xPositions.push(element.x);
-								yPositions.push(element.y);
-
-
-								//Include any colour information about the element
-								tooltipLabels.push(helpers.template(this.options.multiTooltipTemplate, element));
-								tooltipColors.push({
-									fill: element._saved.fillColor || element.fillColor,
-									stroke: element._saved.strokeColor || element.strokeColor
-								});
-
-							}, this);
-
-							yMin = min(yPositions);
-							yMax = max(yPositions);
-
-							xMin = min(xPositions);
-							xMax = max(xPositions);
-
-							return {
-								x: (xMin > this.chart.width/2) ? xMin : xMax,
-								y: (yMin + yMax)/2
-							};
-						}).call(this, dataIndex);
-
-					new Chart.MultiTooltip({
-						x: medianPosition.x,
-						y: medianPosition.y,
-						xPadding: this.options.tooltipXPadding,
-						yPadding: this.options.tooltipYPadding,
-						xOffset: this.options.tooltipXOffset,
-						fillColor: this.options.tooltipFillColor,
-						textColor: this.options.tooltipFontColor,
-						fontFamily: this.options.tooltipFontFamily,
-						fontStyle: this.options.tooltipFontStyle,
-						fontSize: this.options.tooltipFontSize,
-						titleTextColor: this.options.tooltipTitleFontColor,
-						titleFontFamily: this.options.tooltipTitleFontFamily,
-						titleFontStyle: this.options.tooltipTitleFontStyle,
-						titleFontSize: this.options.tooltipTitleFontSize,
-						cornerRadius: this.options.tooltipCornerRadius,
-						labels: tooltipLabels,
-						legendColors: tooltipColors,
-						legendColorBackground : this.options.multiTooltipKeyBackground,
-						title: ChartElements[0].label,
-						chart: this.chart,
-						ctx: this.chart.ctx,
-						custom: this.options.customTooltips
-					}).draw();
-
-				} else {
-					helpers.each(ChartElements, function(Element) {
-						var tooltipPosition = Element.tooltipPosition();
-						new Chart.Tooltip({
-							x: Math.round(tooltipPosition.x),
-							y: Math.round(tooltipPosition.y),
-							xPadding: this.options.tooltipXPadding,
-							yPadding: this.options.tooltipYPadding,
-							fillColor: this.options.tooltipFillColor,
-							textColor: this.options.tooltipFontColor,
-							fontFamily: this.options.tooltipFontFamily,
-							fontStyle: this.options.tooltipFontStyle,
-							fontSize: this.options.tooltipFontSize,
-							caretHeight: this.options.tooltipCaretSize,
-							cornerRadius: this.options.tooltipCornerRadius,
-							text: helpers.template(this.options.tooltipTemplate, Element),
-							chart: this.chart,
-							custom: this.options.customTooltips
-						}).draw();
-					}, this);
-				}
-			}
 			return this;
 		},
 		draw : function(easeDecimal){
